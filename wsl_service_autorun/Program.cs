5cfd88fed6bfffe7d79ee2c565b34bac9c8fbd49
Neon <toyzz@live.com>
2018-05-03 16:49:11 -0700

    Add project files.

---
+++ wsl_service_autorun/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsl_service_autorun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
